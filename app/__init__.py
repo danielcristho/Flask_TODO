7202476d239695c4e8c5b3df92cae387e486717f
Daniel Pepuho <69733783+danielcristho@users.noreply.github.com>
2022-03-06 18:19:06 +0900

    Delete app directory

--- app/__init__.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
-
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///task.db'
-db = SQLAlchemy(app)
-
-from app import routes
