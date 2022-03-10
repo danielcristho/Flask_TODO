b6285f30324681e3f16e75c60d23435a84e1f58e
danielcristho <pepuhodaniel93.learn@gmail.com>
2022-03-10 10:58:44 +0900

    update config

--- flask/app/__init__.py
+++ flask/app/__init__.py
@@ -1,8 +1,11 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+from app.config import DEV_DB, PROD_DB
+import os
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///task.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = DEV_DB
+
 db = SQLAlchemy(app)
 
 from app import routes
