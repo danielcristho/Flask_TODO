9c3b936a46f90ae39b5fb74b801ede4bb5ce59a4
danielcristho <pepuhodaniel93.learn@gmail.com>
2024-12-09 23:50:40 +0700

    feat: update flask & docker compose

---
+++ flask/run.py
@@ -0,0 +1,8 @@
+from app import app, db
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    print("All tables created successfully!")
+    #app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run()
