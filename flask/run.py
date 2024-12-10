e78eb0046321665d739b850d015c968086118f39
danielcristho <pepuhodaniel93.learn@gmail.com>
2024-12-10 11:06:53 +0700

    feat: update port config

--- flask/run.py
+++ flask/run.py
@@ -4,5 +4,5 @@ if __name__ == '__main__':
     with app.app_context():
         db.create_all()
     print("All tables created successfully!")
-    #app.run(debug=True, host='0.0.0.0', port=5000)
-    app.run()
+    app.run(debug=True, host='0.0.0.0', port=5000)
+    # app.run()
